d6698074412f1fe42cc7ee3e500f151114381a44
Michael James <micrjames921@gmail.com>
2024-02-24 14:28:09 -0700

    fixed rebasing issue

--- src/main/java/com/example/quizapp/controller/QuestionController.java
+++ src/main/java/com/example/quizapp/controller/QuestionController.java
@@ -1,33 +1,17 @@
 package com.example.quizapp.controller;
 
-<<<<<<< HEAD
-import java.util.List;
-// import com.example.quizapp.Question;
-import com.example.quizapp.service.QuestionService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-=======
 import com.example.quizapp.service.QuestionService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.beans.factory.annotation.Autowired;
->>>>>>> 2230d4a (created basic files for app)
 
 @RestController
 @RequestMapping("question")
 public class QuestionController {
    @Autowired
    QuestionService questionService;
-<<<<<<< HEAD
-
    @GetMapping("allQuestions")
-   // public List<Question> getAllQuestions() {
-=======
-   @GetMapping("allQuestions")
->>>>>>> 2230d4a (created basic files for app)
    public String getAllQuestions() {
 	  return questionService.getAllQuestions();
    }
--- src/main/java/com/example/quizapp/service/QuestionService.java
+++ src/main/java/com/example/quizapp/service/QuestionService.java
@@ -1,21 +1,12 @@
 package com.example.quizapp.service;
 
-<<<<<<< HEAD
 import com.example.quizapp.Question;
 import org.springframework.stereotype.Service;
 
 @Service
 public class QuestionService {
 
-   // public List<Question> getAllQuestions() {
-   public String getAllQuestions() {
-	  return "Here are the questions!";
-=======
-import org.springframework.stereotype.Service;
-@Service
-public class QuestionService {
    public String getAllQuestions() {
 	  return "These are some questions.";
->>>>>>> 2230d4a (created basic files for app)
    }
 }
